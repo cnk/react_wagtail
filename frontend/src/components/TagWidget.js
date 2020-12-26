17d9cc85aa4fe09df280648a6f23689540fa4d26
Cynthia Kiser <cynthiakiser@gmail.com>
2020-12-25 16:28:55 -0800

    Make our first Component and it's Storybook page

---
+++ frontend/src/components/TagWidget.js
@@ -0,0 +1,47 @@
+import React from "react";
+import axios from "axios";
+import '../index.scss';
+
+class TagWidget extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tags: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    axios.get("/api/blog/tags/").then((res) => {
+      const tags = res.data.results;
+      this.setState({
+        tags,
+        loading: false
+      });
+    });
+  }
+
+  render() {
+    let content;
+    if (this.state.loading) {
+      content = 'Loading...';
+    } else {
+      content = this.state.tags.map((tag) => (
+        <a href={`/tag/${tag.slug}`} key={tag.slug}>
+          <span className="badge badge-secondary">{tag.name}</span>{" "}
+        </a>
+      ))
+    }
+
+    return (
+      <div className="card my-4">
+        <h5 className="card-header">Tags</h5>
+        <div className="card-body">
+          {content}
+        </div>
+      </div>
+    );
+  }
+}
+
+export { TagWidget };
---
+++ frontend/src/stories/mockUtils.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+
+const mockTag = (mockAxios) => {
+  const API_REQUEST = "/api/blog/tags/";
+  mockAxios.onGet(API_REQUEST).reply(200, {
+    results: [
+      {slug: "django", name: "Django"},
+      {slug: "react", name: "React"},
+      {slug: "wagtail", name: "Wagtail"},
+    ],
+  });
+};
+
+export { mockTag };
