4d561573d3e280a0b1b6bf82030a14c2e13a08e8
Cynthia Kiser <cnk@caltech.edu>
2021-01-02 17:00:08 -0800

    Moved PagePreviewAPIViewSet to core where I have the rest of the WagtailAPI viewsets

--- react_wagtail_app/core/api.py
+++ react_wagtail_app/core/api.py
@@ -1,8 +1,40 @@
-# from rest_framework import routers
+from django.contrib.contenttypes.models import ContentType
+from rest_framework.response import Response
+from wagtail_headless_preview.models import PagePreview
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.api.v2.views import PagesAPIViewSet
 from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from wagtail.images.api.v2.views import ImagesAPIViewSet
+
+
+class PagePreviewAPIViewSet(PagesAPIViewSet):
+    known_query_parameters = PagesAPIViewSet.known_query_parameters.union(
+        ["content_type", "token"]
+    )
+
+    def listing_view(self, request):
+        page = self.get_object()
+        serializer = self.get_serializer(page)
+        return Response(serializer.data)
+
+    def detail_view(self, request, pk):
+        page = self.get_object()
+        serializer = self.get_serializer(page)
+        return Response(serializer.data)
+
+    def get_object(self):
+        app_label, model = self.request.GET["content_type"].split(".")
+        content_type = ContentType.objects.get(app_label=app_label, model=model)
+
+        page_preview = PagePreview.objects.get(
+            content_type=content_type, token=self.request.GET["token"]
+        )
+        page = page_preview.as_page()
+        if not page.pk:
+            # fake primary key to stop API URL routing from complaining
+            page.pk = 0
+
+        return page
 
 
 cms_api_router = WagtailAPIRouter("api")
@@ -15,3 +47,4 @@ cms_api_router = WagtailAPIRouter("api")
 cms_api_router.register_endpoint("pages", PagesAPIViewSet)
 cms_api_router.register_endpoint("images", ImagesAPIViewSet)
 cms_api_router.register_endpoint("documents", DocumentsAPIViewSet)
+cms_api_router.register_endpoint("page_preview", PagePreviewAPIViewSet)
